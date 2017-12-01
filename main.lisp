aab12e2837718b1fe7d3c8274cd99653d8bf974b
t-sin <shinichi.tanaka45@gmail.com>
2017-12-01 14:04:07 +0900

    Bulk writing portablly
    Thanks @snmsts
    https://github.com/snmsts/cl-yes/commit/2caa9e301b23d38282cdc69e37d04ae8f2f949ab

--- main.lisp
+++ main.lisp
@@ -4,6 +4,12 @@
   (:export :yes))
 (in-package :cl-yes)
 
+(defun make-buffer (s &optional (times 1000000))
+  (with-output-to-string (out)
+    (dotimes (n times)
+      (write-line s out))))
+
 (defun yes (out &optional (s "y"))
-  (loop
-     (write-line s out)))
+  (let ((buf (make-buffer s)))
+    (loop
+       (princ buf out))))
